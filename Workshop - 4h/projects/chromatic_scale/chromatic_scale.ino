97ccdf99b200e0f454b17740bc880ce9b924763d
Gustavo Silveira <silveira.go@gmail.com>
2018-07-26 23:33:25 -0400

    update

--- Workshop - 4h/projects/chromatic_scale/chromatic_scale.ino
+++ Workshop - 4h/projects/chromatic_scale/chromatic_scale.ino
@@ -5,18 +5,18 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 // Variables are containers of a certain type. INT variables are containers that store an integer number. We use them so the code is more readable.
 // Variables can be used values that won't change, like the pin of a potenciometer, or values that will change, like the reading of this potentiometer
 int ledPin = 13;
-int delayTime = 30;
+int delayTime = 40;
 
 void setup() { // happens once when the Arduino starts
   
   Serial.begin(31250); // For Serial communication. MIDI baud rate: 31250 - Serial baud rate: 9600
-  pinMode(ledPin, OUTPUT); //dealcres a digital pin as an output, for leds, motors, etc
+  pinMode(ledPin, OUTPUT); //declares a digital pin as an output, for leds, motors, etc
 
 }
 
 void loop() { // loops forever
   
-  for (int i = 36; i < 56; i++) { 
+  for (int i = 24; i < 90; i++) { 
     // creates a loop that will occur X times (48), 
     // where "i" will be incremented by 1 every loop (i++) and it starts with 36
     
